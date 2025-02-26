31a8ca7065bd49d4198368f017f7df64b2423a42
Evgeniya Pronko <shreidereva@gmail.com>
2025-02-26 17:50:43 +0300

    feat: solution task 7.1; update: solution task 4

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -1,7 +1,50 @@
-import fetchJson from './utils/fetch-json.js';
+import fetchJson from "./utils/fetch-json.js";
+import ColumnChartV1 from "../../04-oop-basic-intro-to-dom/1-column-chart/index.js";
 
-const BACKEND_URL = 'https://course-js.javascript.ru';
+const BACKEND_URL = "https://course-js.javascript.ru";
 
-export default class ColumnChart {
+export default class ColumnChart extends ColumnChartV1 {
+  url;
+  constructor({
+    url = "",
+    range = {},
+    label = "",
+    link = "",
+    formatHeading = (value) => value,
+  } = {}) {
+    super({ label, link, formatHeading });
+    this.url = url;
+    const { from, to } = range;
+    this.fetchData(from, to);
+  }
 
+  createUrl(from, to) {
+    const url = new URL(this.url, BACKEND_URL);
+    url.searchParams.append('from', from);
+    url.searchParams.append('to', to);
+    return url.toString();
+  }
+  fetchData(from, to) {
+    fetch(this.createUrl(from, to))
+      .then(response => response.json())
+      .then(data => {
+        this.data = Object.values(data);
+        this.value = this.data.reduce((sum, current) => sum + current, 0);
+        super.update(this.data);
+      })
+      .catch(error => console.error(error));
+  }
+
+  async update(from, to) {
+    try {
+      const url = this.createUrl(from, to);
+      const response = await fetchJson(url);
+      this.data = Object.values(response);
+      this.value = this.data.reduce((sum, current) => sum + current, 0);
+      super.update(this.data);
+      return response;
+    } catch (error) {
+      console.error(error);
+    } 
+  }
 }
